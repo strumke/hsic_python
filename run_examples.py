b7289e1c18ee00187ee6f4aec7acc2f89a220e91
Strumke <inga@strumke.com>
2020-04-22 17:51:51 +0200

    Added code for Danny to scrutinise

---
+++ run_examples.py
@@ -0,0 +1,22 @@
+import numpy
+from hsic import dHSIC
+
+# --- Number of variables and samples
+D = 5
+N = 1000
+
+# --- Simulate distribution
+X = numpy.array([numpy.linspace(-1, 1, N) for _ in range(D)]).T
+TWO_D = 2*numpy.array(range(D))
+Y = numpy.matmul(numpy.multiply(X, X), TWO_D)
+# ---
+
+# --- Run dHSIC calculation
+# --- (if no parameters have been changed, this will return 0.05190...
+print(dHSIC(X, Y))
+
+# --- Generate several sets of random data
+X1 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
+X2 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
+Y = numpy.matmul(numpy.multiply(X, X), TWO_D)
+print(dHSIC(X1, X2, Y))
