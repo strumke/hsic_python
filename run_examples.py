33c4fe8a60151f2f8fe90e7a65a7d45fff3c5f08
Strumke <inga@strumke.com>
2020-05-01 11:38:35 +0200

    Finished examples

--- run_examples.py
+++ run_examples.py
@@ -20,3 +20,8 @@ X1 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
 X2 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
 Y = numpy.matmul(numpy.multiply(X1, X2), TWO_D)
 print(dHSIC(X1, X2, Y))
+
+# or, if you want to pass your arguments as a list, unpack it:
+print(dHSIC(*[X1, X2, Y]))
+
+print("ok! Good to go.")
