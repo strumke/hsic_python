048b47718a851bdc475ff6c3ab12ad268a3f9299
Strumke <inga@strumke.com>
2020-05-01 11:33:11 +0200

    Finished examples

--- run_examples.py
+++ run_examples.py
@@ -18,5 +18,5 @@ print(dHSIC(X, Y))
 # --- Generate several sets of random data
 X1 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
 X2 = numpy.array([numpy.random.uniform(-1, 1, N) for _ in range(D)]).T
-Y = numpy.matmul(numpy.multiply(X, X), TWO_D)
+Y = numpy.matmul(numpy.multiply(X1, X2), TWO_D)
 print(dHSIC(X1, X2, Y))
